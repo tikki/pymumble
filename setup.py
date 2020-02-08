7f6412f50f4c2bef39282c71735e9605ba744b00
tikki <tikki@users.noreply.github.com>
2020-02-08 21:48:00 +0100

    drop support for py<3.6

--- setup.py
+++ setup.py
@@ -17,10 +17,6 @@ setup(
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
     ],
 )
